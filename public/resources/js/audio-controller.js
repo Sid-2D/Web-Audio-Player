6a6b1aa2eac9e956a2562740d98397606495e341
Siddharth Chopra <sid6327@gmail.com>
2018-01-07 22:26:33 +0530

    Adding controls and layout for audio.

--- public/resources/js/audio-controller.js
+++ public/resources/js/audio-controller.js
@@ -5,6 +5,37 @@ let audioController = (() => {
        currentSong: null
    }
    
-   
-   
+   function addEffects(audio) {
+       audio.onplay()
+   }
+    
+   return { 
+       audioContext,
+       
+       setCurrentSong: (audio) => {
+           data.currentSong = audio
+           addEffects(audio)
+       },
+       
+       pauseSong: () => {
+           data.currentSong.pause()
+           data.currentSong.currentTime = 0
+       },
+       
+       getCurrentSong: () => {
+           return data.currentSong
+       },
+       
+       addListeners: (audio, gainNode) => {
+           // Add onplay event and play
+            audio.onplay = () => {
+                gainNode.gain.exponentialRampToValueAtTime(1, audioContext.currentTime + 1.5)
+                if (data.currentSong && data.currentSong != audio) {
+                    pauseSong()
+                    setCurrentSong(audio)
+                    fillRelated(audio.id)
+                }
+            }
+       }
+   }
 })()
